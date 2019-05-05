e8bc497bc8748690260a4abfffd49a0a280981e5
AkhtarHameed <sp19bscs0098@maju.edu.pk>
2019-05-05 16:30:13 +0500

    Piano music

---
+++ play.py
@@ -0,0 +1,13 @@
+import RPI.GPIO as GPIO 
+import pygame 
+pygame.init()
+
+paino_tile_1 = pygame.mixer.Sound
+("C:\Users\cc\Downloads\Music_Notes\C_S.wav")
+
+btn1.when_pressed = print('btn1')
+btn2.when_pressed = print('btn1')
+btn3.when_pressed = print('btn1')
+btn4.when_pressed = print('btn1')
+btn5.when_pressed = print('btn1')
+btn6.when_pressed = print('btn1')
