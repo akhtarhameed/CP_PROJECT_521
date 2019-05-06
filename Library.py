636e559ac654447a944abf29eb508945209ee0f7
AkhtarHameed <sp19bscs0098@maju.edu.pk>
2019-05-06 19:13:10 +0500

    Python's Library

---
+++ Library.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.locals import *
+from mingus.core import notes, chords
+from mingus.containers import *
+from mingus.midi import fluidsynth
+from os import sys
+SF2 = 'soundfont.sf2'
